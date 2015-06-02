2799ead60f9ee9fab6e8bdb144d26f283a44f1f0
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2015-06-02 23:01:36 +0300

    initial commit

--- server.rb
+++ server.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require_relative 'mpu6050'
 
 configure do
-  set :mpu, MPU6050.new '/home/pi/wiringPi/wiringPi/libwiringPi.so.2.25'
+  set :mpu, MPU6050.new('/home/pi/wiringPi/wiringPi/libwiringPi.so.2.25')
 end
 
 get '/' do
