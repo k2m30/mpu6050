eac93d45d3bfcaed452d2a069ee88d18c7082d7a
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2015-06-02 22:54:35 +0300

    initial commit

---
+++ server.rb
@@ -0,0 +1,10 @@
+require 'sinatra'
+require_relative 'mpu6050'
+
+configure do
+  set :mpu, MPU6050.new '/home/pi/wiringPi/wiringPi/libwiringPi.so.2.25'
+end
+
+get '/' do
+  settings.mpu.measure
+end
