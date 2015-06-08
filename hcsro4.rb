0608367e2cc12866dbfbd45a27be71f62291ad46
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2015-06-09 01:25:49 +0300

    hcsro4 detector added

--- hcsro4.rb
+++ hcsro4.rb
@@ -22,6 +22,9 @@ class HCSRO4
     # extern int  wiringPiSetup       (void) ;
     @setup = Fiddle::Function.new(wiringpi['wiringPiSetup'], [void], int)
 
+    # extern int  wiringPiSetupGpio       (void) ;
+    @setup_gpio = Fiddle::Function.new(wiringpi['wiringPiSetupGpio'], [void], int)
+
     # extern void pinMode             (int pin, int mode) ;
     @pin_mode = Fiddle::Function.new(wiringpi['pinMode'], [int, int], void)
 
@@ -31,24 +34,91 @@ class HCSRO4
     # extern void digitalWrite        (int pin, int value) ;
     @digital_write = Fiddle::Function.new(wiringpi['digitalWrite'], [int, int], void)
 
-    @setup.call nil
+    result = @setup_gpio.call nil
     @pin_mode.call TRIG, OUT
     @pin_mode.call ECHO, IN
+    result
   end
 
   def measure
-    # @digital_write.call TRIG, LOW
-    # sleep DELAY
+    # GPIO.output(TRIG, False)                 #Set TRIG as LOW
+    # print "Waitng For Sensor To Settle"
+    # time.sleep(2)                            #Delay of 2 seconds
+    #
+    # GPIO.output(TRIG, True)                  #Set TRIG as HIGH
+    # time.sleep(0.00001)                      #Delay of 0.00001 seconds
+    # GPIO.output(TRIG, False)                 #Set TRIG as LOW
+    #
+    # while GPIO.input(ECHO)==0:               #Check whether the ECHO is LOW
+    #   pulse_start = time.time()              #Saves the last known time of LOW pulse
+    #
+    #   while GPIO.input(ECHO)==1:               #Check whether the ECHO is HIGH
+    #     pulse_end = time.time()                #Saves the last known time of HIGH pulse
+    #
+    #     pulse_duration = pulse_end - pulse_start #Get pulse duration to a variable
+    #
+    #     distance = pulse_duration * 17150        #Multiply pulse duration by 17150 to get distance
+    #     distance = round(distance, 2)
 
-    @digital_write.call TRIG, HIGH
-    sleep 0.00001
-    @digital_write.call TRIG, LOW
-    start = Time.now
+    require_relative 'hcsro4'
+
+
+    start = nil
     finish = nil
 
-    while @digital_read.call(ECHO) == 0 do
-      finish = Time.now
+    # a = HCSRO4.new('/home/pi/wiringPi/wiringPi/libwiringPi.so.2.25')
+
+    pin = 27
+    File.open("/sys/class/gpio/export", "w") { |f| f.write("#{pin}") }
+    File.open("/sys/class/gpio/gpio#{pin}/direction", "w") { |f| f.write("in") }
+
+    pin = 17
+    File.open("/sys/class/gpio/export", "w") { |f| f.write("#{pin}") }
+    File.open("/sys/class/gpio/gpio#{pin}/direction", "w") { |f| f.write("out") }
+
+
+    zero = Time.now.to_f
+    pin = 17
+    value = 1
+    File.open("/sys/class/gpio/gpio#{pin}/value", 'w') {|f| f.write("#{value}") }
+
+    sleep 0.01
+    pin = 17
+    value = 0
+    File.open("/sys/class/gpio/gpio#{pin}/value", 'w') {|f| f.write("#{value}") }
+
+
+    while File.read("/sys/class/gpio/gpio27/value").to_i == 0 do
+      p 0
     end
+
+    while File.read("/sys/class/gpio/gpio27/value").to_i == 1 do
+      p 1
+    end
+
+
+    # while a.digital_read.call(27) == 0  && (start - zero) < 100000 && (start - zero) > 0 do
+    #   start = Time.now.to_f
+    # end
+    #
+    # p [a.digital_read.call(27), zero - start]
+    #
+    # while a.digital_read.call(27) == 1 do
+    #   finish = Time.now.usec
+    #   break if finish - zero > 1000
+    # end
+    #
+    # finish - start
+
+# @digital_write.call TRIG, HIGH
+#     sleep 0.00001
+#     @digital_write.call TRIG, LOW
+#     start = Time.now
+#     finish = nil
+#
+#     while @digital_read.call(ECHO) == 0 do
+#       finish = Time.now
+#     end
 
     (finish - start)/58
   end
